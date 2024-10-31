181af2824215478bc5cfca67aa3a1298d8233d16
CodesOfVysakh <onvysakh309@gmail.com>
2024-10-31 13:33:41 +0530

    Initial Commit

---
+++ controls.php
@@ -0,0 +1,8 @@
+<?php
+    $score = 75;
+    if($score >= 60){
+        echo "PASS";
+    }else{
+        echo "FAIL";
+    }
+?>
---
+++ variables.php
@@ -0,0 +1,5 @@
+<?php
+    $name = "Alice";
+    $age = 25;
+    echo "My name is $name and I am $age years old."
+?>
